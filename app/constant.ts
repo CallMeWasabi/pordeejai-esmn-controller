b29808c1d300dbc1adae4c9cece468f5f6233910
HotWasabiSushi <76900409+CallMeWasabi@users.noreply.github.com>
2024-01-05 22:08:14 +0700

    change api url

--- app/constant.ts
+++ app/constant.ts
@@ -2,5 +2,5 @@
 
 // export const clientWebserverUrl = "https://7712-101-108-69-171.ngrok-free.app"
 // export const clientWebserverUrl = "https://a760-101-108-69-171.ngrok-free.app"
-export const clientWebserverUrl = "https://bc16-2a09-bac5-56bb-18c8-00-278-32.ngrok-free.app"
+export const clientWebserverUrl = "http://192.168.1.15:8080"
 export const shopUrl = "https://pordeejai-esmn-frontend.vercel.app"
