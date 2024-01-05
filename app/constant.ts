76b06aef3b80f321be2969fd4a990c5f355e34fd
HotWasabiSushi <76900409+CallMeWasabi@users.noreply.github.com>
2024-01-05 21:18:22 +0700

    add

--- app/constant.ts
+++ app/constant.ts
@@ -2,5 +2,5 @@
 
 // export const clientWebserverUrl = "https://7712-101-108-69-171.ngrok-free.app"
 // export const clientWebserverUrl = "https://a760-101-108-69-171.ngrok-free.app"
-export const clientWebserverUrl = "http://localhost:8080"
-export const shopUrl = "http://localhost:3001"
+export const clientWebserverUrl = "https://4807-2a09-bac5-56bb-18c8-00-278-32.ngrok-free.app"
+export const shopUrl = "https://pordeejai-esmn-frontend.vercel.app"
